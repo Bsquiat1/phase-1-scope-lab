df946a0ee2e4830c059eaf828fc9b828ff9f797c
Denis <denis.were@student.moringaschool.com>
2023-03-19 22:36:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function returnCustomerName() {
+  return customerName;
+}
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4) Overwrite the best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a constant called leastFavoriteCustomer and assign it some initial value
+const leastFavoriteCustomer = 'Karen';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
